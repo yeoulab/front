7b2175f6235d46dc56be07519b9f0eb703e0fd04
Yeoulab <myungjin.shin@gmail.com>
2018-12-19 19:30:54 +0900

    register vue changed

--- src/register/RegisterPage.vue
+++ src/register/RegisterPage.vue
@@ -1,34 +1,40 @@
 <template>
     <div>
-        <h2>Register</h2>
-        <form @submit.prevent="handleSubmit">
-            <div class="form-group">
-                <label for="firstName">First Name</label>
-                <input type="text" v-model="user.firstName" v-validate="'required'" name="firstName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
-                <div v-if="submitted && errors.has('firstName')" class="invalid-feedback">{{ errors.first('firstName') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="lastName">Last Name</label>
-                <input type="text" v-model="user.lastName" v-validate="'required'" name="lastName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
-                <div v-if="submitted && errors.has('lastName')" class="invalid-feedback">{{ errors.first('lastName') }}</div>
-            </div>
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" v-model="user.username" v-validate="'required'" name="username" class="form-control" :class="{ 'is-invalid': submitted && errors.has('username') }" />
-                <div v-if="submitted && errors.has('username')" class="invalid-feedback">{{ errors.first('username') }}</div>
-            </div>
-            <div class="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
-                <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div>
-            </div>
-            <div class="form-group">
-                <button class="btn btn-primary" :disabled="status.registering">Register</button>
+        <h4>회원가입</h4>
+        <!-- <form @submit.prevent="handleSubmit"> -->
+            <!-- <div class="form-group"> -->
+                <!-- <label for="firstName">First Name</label> -->
+                <input type="text" v-model="user.firstName" v-validate="'required'" name="firstName" placeholder="First Name" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
+                <!-- <div v-if="submitted && errors.has('firstName')" class="invalid-feedback">{{ errors.first('firstName') }}</div> -->
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
+                <!-- <label for="lastName">Last Name</label> -->
+                <input type="text" v-model="user.lastName" v-validate="'required'" name="lastName" placeholder="Last Name" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
+                <!-- <div v-if="submitted && errors.has('lastName')" class="invalid-feedback">{{ errors.first('lastName') }}</div> -->
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
+                <!-- <label for="username">ID</label> -->
+                <input type="text" v-model="user.username" v-validate="'required'" name="username" placeholder="ID" class="form-control" :class="{ 'is-invalid': submitted && errors.has('username') }" />
+                <!-- <div v-if="submitted && errors.has('username')" class="invalid-feedback">{{ errors.first('username') }}</div> -->
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
+                <!-- <label htmlFor="password">Password</label> -->
+                <input type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" placeholder="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
+                <p></p>
+                <!-- <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div> -->
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
+                <!-- <button class="btn btn-primary" :disabled="status.registering">Register</button> -->
+                <button class="btn btn-lg btn-primary btn-block" type="submit" v-on:click="userRegist">가입</button>
                 <img v-show="status.registering" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                <router-link to="/login" class="btn btn-link">Cancel</router-link>
-            </div>
-        </form>
-        <button id="signin-button" @click="handleSignInClick()">구글로 회원가입</button>
+                <router-link to="/login" class="btn btn-link">뒤로가기</router-link>
+                <p></p>
+            <!-- </div> -->
+        <!-- </form> -->
+        <!-- <button id="signin-button" @click="handleSignInClick()">구글로 회원가입</button> -->
+        <a @click="handleSignInClick()">
+            <img src="https://s3.ap-northeast-2.amazonaws.com/diary-image/btn_google_signin_light_normal_web.png">
+        </a>
     </div>
 </template>
 
@@ -52,6 +58,28 @@ export default {
     },
     methods: {
         ...mapActions('account', ['register']),
+        userRegist(e){
+            console.log("userRegist start");
+            console.log(this.user);
+            if( !this.user.firstName ){
+                alert("이름을 입력하세요");
+                return;
+            }
+            if( !this.user.lastName ){
+                alert("성을 입력하세요");
+                return;
+            }
+            if( !this.user.username ){
+                alert("ID를 입력하세요");
+                return;
+            }
+            if( !this.user.password ){
+                alert("비밀번호를 입력하세요");
+                return;
+            }
+
+            this.register(this.user);
+        },
         handleSubmit(e) {
             this.submitted = true;
             this.$validator.validate().then(valid => {
