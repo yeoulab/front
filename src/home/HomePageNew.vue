e5c8329c11d7c24f37bf05757b50c7b2e2d307e8
yeoulab <44544935+yeoulab@users.noreply.github.com>
2019-01-25 13:49:49 +0900

    Update HomePageNew.vue

--- src/home/HomePageNew.vue
+++ src/home/HomePageNew.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
-    <TodoDate></TodoDate>
-    <GetTodo></GetTodo>
-    <br>
-    <CurrentTodos></CurrentTodos>
-    <br>
-    <CompletedTodos></CompletedTodos>
-    <br>
-    <Confirm></Confirm>
-    <br>
-
-    <!-- <div><b-button to="/login">로그아웃</b-button></div> -->
-    <!--<p>{{ user }}</p>-->
+  <div class="todoList">
+    <!-- <div class="menu">
+      <Navigation></Navigation>
+    </div> -->
+    <div class="todoChart">
+      <TodoStatistic></TodoStatistic>
+    </div>
+    <div class="todo">
+      <TodoDate></TodoDate>
+      <GetTodo></GetTodo>
+      <br>
+      <CurrentTodos></CurrentTodos>
+      <br>
+      <CompletedTodos></CompletedTodos>
+      <br>
+      <Confirm></Confirm>
+      <br>
+    </div>
+    <div class="topMenu">
+      <TopMenu></TopMenu>
+    </div>
   </div>
 </template>
 <script>
@@ -20,25 +28,63 @@ import CurrentTodos from './todo/CurrentTodos.vue'
 import CompletedTodos from './todo/CompletedTodos.vue'
 import TodoDate from './todo/TodoDate.vue'
 import Confirm from './todo/Confirm.vue'
+import Navigation from './Navigation.vue'
+import TopMenu from './TopMenu.vue'
+import TodoStatistic from './todo/TodoStatistic.vue'
 import { mapState } from 'vuex';
 
 export default {  
+
   components: {
     GetTodo,
     CurrentTodos,
     CompletedTodos,
     TodoDate,
     Confirm,
+    Navigation,
+    TopMenu,
+    TodoStatistic,
   },
   computed: {
     ...mapState({
       user: state => state.account.user,
     })
-  }
+  },
 }
 </script>
 <style>
-body {
-  font-family: Helvetica, sans-serif;
+  body {
+    font-family: Helvetica, sans-serif;
+  }
+
+.topMenu{
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0%;
 }
+
+.todo{
+  position:absolute;
+  width: 300px;
+  top: 70px;
+  left: 220px;
+  /* top:50%; */
+  /* left:50%; */
+  /* transform:translate(-50%, 0%) */
+}
+.todoChart{
+  position:absolute;
+  width: 200px;
+  top: 70px;
+  left: 50px;
+}
+
+.menu{
+  position:absolute;
+  width: 200px;
+  top: 70px;
+  left: 0%;
+}
+
 </style>
