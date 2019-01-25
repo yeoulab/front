a497d50456e3356137d9ec63e7ad4378d7bb1cb7
yeoulab <44544935+yeoulab@users.noreply.github.com>
2019-01-25 13:46:27 +0900

    Update router.js

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -8,34 +8,36 @@ import RegisterPage from '../register/RegisterPage'
 // new homepage
 import HomePageNew from '../home/HomePageNew'
 
+import FirstPage from '../home/FirstPage'
+
 // router 에 대한 config
 Vue.use(Router);
 
 export const router = new Router({
-  mode: 'history',
-  routes: [
-    { path: '/', component: HomePageNew }, // home 인 경우 Homepage vue 로 렌더링
-    { path: '/login', component: LoginPage }, // login 인 경우 LoginPage Vue 로 렌더링
-    { path: '/register', component: RegisterPage }, // register 인 경우 RegisterPage Vue 로 렌더링
-
-    // otherwise redirect to home
-    { path: '*', redirect: '/' }
-  ]
+    mode: 'history',
+    routes: [
+        { path: '/home', component: HomePageNew }, // home 인 경우 Homepage vue 로 렌더링
+        { path: '/login', component: LoginPage }, // login 인 경우 LoginPage Vue 로 렌더링
+        { path: '/register', component: RegisterPage }, // register 인 경우 RegisterPage Vue 로 렌더링
+        { path: '/', component: FirstPage },
+        // otherwise redirect to home
+        { path: '*', redirect: '/' }
+    ]
 });
 
 // 라우팅 되기전에 발생하는 이벤트
 router.beforeEach((to, from, next) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  // 아래 publicPages 의 렌더링할 페이지 ( to.path ) 가 존재하는지 확인
-  // 존재 한다면, login 인증이 된것.
-  const publicPages = ['/login', '/register'];
-  const authRequired = !publicPages.includes(to.path);
-  const loggedIn = localStorage.getItem('user');
+    // redirect to login page if not logged in and trying to access a restricted page
+    // 아래 publicPages 의 렌더링할 페이지 ( to.path ) 가 존재하는지 확인
+    // 존재 한다면, login 인증이 된것.
+    const publicPages = ['/login', '/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
 
-  // 로그인이 안된경우, 무조건 /login 으로 렌더링
-  if (authRequired && !loggedIn) {
-    return next('/login');
-  }
+    // 로그인이 안된경우, 무조건 /login 으로 렌더링
+    //if (authRequired && !loggedIn) {
+    //  return next('/login');
+    //}
 
-  next();
+    next();
 })
