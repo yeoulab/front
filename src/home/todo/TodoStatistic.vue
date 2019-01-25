cb17590842b02c0f7d3f5e6ef0543b9a4fa42fb9
yeoulab <44544935+yeoulab@users.noreply.github.com>
2019-01-25 13:55:31 +0900

    Create TodoStatistic.vue

---
+++ src/home/todo/TodoStatistic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-progress type="circle" :percentage=todoRates></el-progress>
+    <!-- <label>오늘의 진도</label> -->
+  </div>
+</template>
+<script>
+// vuex 를 위한 import
+import {mapState, mapActions, mapGetters} from 'vuex';
+
+export default{
+    computed: {
+        ...mapGetters('todos',{
+            todos: 'currentTodo',
+            completedTodos: 'completedTodo',
+            todoRates: 'todoRates',
+        })
+    },
+    methods: {
+
+    }
+}
+</script>
+<style>
+</style>
