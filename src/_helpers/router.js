b761895433b4bd85361d9d985d5bd8acbd14867b
yeoulab <44544935+yeoulab@users.noreply.github.com>
2019-01-28 10:24:38 +0900

    Update router.js

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -30,14 +30,27 @@ router.beforeEach((to, from, next) => {
     // redirect to login page if not logged in and trying to access a restricted page
     // 아래 publicPages 의 렌더링할 페이지 ( to.path ) 가 존재하는지 확인
     // 존재 한다면, login 인증이 된것.
-    const publicPages = ['/login', '/register'];
+    const publicPages = ['/', '/login', '/register', '/home'];
+    // 라우팅 하려는 vue 가 publicPage 에 포함이 되는지
     const authRequired = !publicPages.includes(to.path);
     const loggedIn = localStorage.getItem('user');
 
+    // 첫 페이지로 이동 시, 로그인이 돼 있으면 home 으로 이동
+    if (to.path === "/" && loggedIn) {
+        return next('/home');
+    }
+
+    if (to.path === "/home" && !loggedIn) {
+        return next('/login');
+    }
+
     // 로그인이 안된경우, 무조건 /login 으로 렌더링
-    //if (authRequired && !loggedIn) {
-    //  return next('/login');
-    //}
+    // if (authRequired && !loggedIn) {
+    //     return next('/login');
+    // }
+    // else {
+    //     return next('/');
+    // }
 
     next();
 })
