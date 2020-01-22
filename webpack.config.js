f7859762273e4c457588733925185f6a660a8d79
yeoulab <44544935+yeoulab@users.noreply.github.com>
2020-01-22 13:13:48 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'http://yeoulab.ml/server'
             //apiUrl: 'http://34.85.37.150:4000'
             //apiUrl: 'http://54.180.100.1:4000'
             //apiUrl: 'http://server.yeoulab.ml:4000'
